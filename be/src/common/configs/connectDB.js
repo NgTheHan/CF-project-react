23cb1eff6d2ad1abde9cea191b6424453867b35d
ShinMoVit <nguyenthehan.dev@gmail.com>
2025-07-17 17:50:11 +0700

    add: config

---
+++ be/src/common/configs/connectDB.js
@@ -0,0 +1,10 @@
+import { DB_URI } from "./environments.js";
+import mongoose from "mongoose";
+function connectDB() {
+  mongoose
+    .connect(DB_URI)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+}
+
+export default connectDB;
